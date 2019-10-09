896426e880f3f7f87bd9f9f12303a2ac3037ef05
mkxmkx <1197993121@qq.com>
2019-10-09 20:33:07 +0800

    剑指offer： 树的子结构

---
+++ NOWCODER/树的子结构/src/TreeNode.java
@@ -0,0 +1,14 @@
+/**
+ * @Author: makexin
+ * @Date: 2019/10/919:17
+ */
+public class TreeNode {
+    int val;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    TreeNode(int val)
+    {
+        this.val = val;
+    }
+}
