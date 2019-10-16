183bf3e1ee1bff45416f3816f2da2d67fc556156
mkxmkx <1197993121@qq.com>
2019-10-16 15:43:44 +0800

    复杂链表的复制

---
+++ NOWCODER/复杂链表的复制/src/RandomListNode.java
@@ -0,0 +1,13 @@
+/**
+ * @Author: makexin
+ * @Date: 2019/10/1613:15
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
