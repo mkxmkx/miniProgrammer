188f8c7b9b550ef1a6f5eaa2f33473329b321fc3
mkxmkx <1197993121@qq.com>
2019-10-29 10:23:46 +0800

    二叉树先序遍历：递归版本和非递归版本

--- 经典算法/树的遍历/src/binaryTree/TreeNode.java
+++ 经典算法/树的遍历/src/binaryTree/TreeNode.java
@@ -5,7 +5,7 @@ package binaryTree;
  * @Date: 2019/10/2910:07
  */
 public class TreeNode {
-    public int val;
+    public int val;//都定义为public是为了在main函数中可以直接赋值
     public TreeNode left = null;
     public TreeNode right = null;
     public TreeNode(int val)
