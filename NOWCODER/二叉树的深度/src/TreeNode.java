2a0bd0752ead341ac8b0dbe05efe154ee78bfaae
mkxmkx <1197993121@qq.com>
2019-10-29 10:02:15 +0800

    剑指offer：二叉树的深度

---
+++ NOWCODER/二叉树的深度/src/TreeNode.java
@@ -0,0 +1,13 @@
+/**
+ * @Author: makexin
+ * @Date: 2019/10/299:53
+ */
+public class TreeNode {
+    int val;
+    TreeNode left = null;
+    TreeNode right = null;
+    public TreeNode(int val)
+    {
+        this.val = val;
+    }
+}
