7cc1c7e63f691b70f569e3b64ea07faf99753f38
mkxmkx <1197993121@qq.com>
2019-11-29 13:15:25 +0800

    剑指offer：删除链表中重复的节点

---
+++ NOWCODER/删除链表中重复的节点/src/ListNode.java
@@ -0,0 +1,12 @@
+/**
+ * @Author: makexin
+ * @Date: 2019/11/2912:25
+ */
+public class ListNode {
+    int val;
+    ListNode next = null;
+    ListNode(int val)
+    {
+        this.val = val;
+    }
+}
