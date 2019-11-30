80f06aba084e4be57a3e3be8324a1262927246ba
mkxmkx <1197993121@qq.com>
2019-11-30 16:51:59 +0800

    剑指offer：二叉树的下一个节点

---
+++ NOWCODER/二叉树的下一个节点/src/TreeLinkNode.java
@@ -0,0 +1,15 @@
+/**
+ * @Author: makexin
+ * @Date: 2019/11/3016:19
+ */
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val)
+    {
+        this.val = val;
+    }
+}
