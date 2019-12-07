912bdb676ec86ee6079294048abb7cc79491f816
mkxmkx <1197993121@qq.com>
2019-12-07 21:45:01 +0800

    剑指offer：二叉搜索树的第k个结点

---
+++ NOWCODER/二叉搜索树的第k个结点/src/TreeNode.java
@@ -0,0 +1,13 @@
+/**
+ * @Author: makexin
+ * @Date: 2019/12/720:55
+ */
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+    TreeNode(int val)
+    {
+        this.val = val;
+    }
+}
